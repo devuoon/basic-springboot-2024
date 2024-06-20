9ff9d10e301317c3ff0992e51e69878b8059ba1b
yoonji <dbswl341@naver.com>
2024-06-20 17:13:12 +0900

    0620

--- spring03/backboard/src/main/java/com/devuoon/backboard/service/MemberService.java
+++ spring03/backboard/src/main/java/com/devuoon/backboard/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.devuoon.backboard.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.devuoon.backboard.entity.Member;
+import com.devuoon.backboard.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +15,12 @@ public class MemberService {
   private final MemberRepository MemberRepository;
 
   public Member setMember(String name, String email, String password) {
-    Memeber member = Member.builder().username(username).email(email).build();
+    Member member = Member.builder().username(username).email(email).build();
+
+    BCryptPasswordEncoder pwEncoder = new BCryptPasswordEncoder();
+    member.setPassword(pwdEncoder.encode(password)); //암호화한 값을 DB에 저장
+    this.MemberRepository.save(member);
+
+    return member;
   }
 }
