b13087c7dbb5a8a17933c434e3262b291906fe9e
yoonji <dbswl341@naver.com>
2024-06-20 09:35:07 +0900

    0620

--- spring03/backboard/src/main/java/com/devuoon/backboard/repository/BoardRepository.java
+++ spring03/backboard/src/main/java/com/devuoon/backboard/repository/BoardRepository.java
@@ -3,7 +3,15 @@ package com.devuoon.backboard.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devuoon.backboard.entity.Board;
+import java.util.List;
+
 
 public interface BoardRepository extends JpaRepository<Board, Long>{
   
+    // 메서드명만 잘 만들면 쿼리를 생성하지 않고 JPA 만들어주는 기능
+    Board findByBno(Long bno);
+    // Board findByContent(String content);
+    Board findByTitle(String title);
+    List<Board> findByTitleLike(String title);
+    
 }
