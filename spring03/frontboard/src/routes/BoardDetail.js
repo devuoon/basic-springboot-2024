4c1755080e71be84e7be9728fde25f420570e809
yoonji <dbswl341@naver.com>
2024-07-04 15:43:38 +0900

    0704

--- spring03/frontboard/src/routes/BoardDetail.js
+++ spring03/frontboard/src/routes/BoardDetail.js
@@ -1,30 +1,31 @@
 import { Link ,useParams } from "react-router-dom";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 import axios from 'axios';
 
 function BoardDetail() {
-
-  //const [bno, setBno] = useState(0);
-
+  const [board, setBoard] = useState(0);
+  var bno, title, content, createDate, modifyDate, writer;
+  var replyList = [];
   const params = useParams();
-  console.log(params.bno);
 
   const getBoardDetail = async (bno) => {
     const resp = (await axios.get("http://localhost:8080/api/board/detail/" + bno));
 
     const resultCode = resp.data.resultCode;
-    console.log(resultCode);
-    console.log(resp.data.data);
+    if (resultCode == "OK") {
+      setBoard(resp.data.data);
+    }
   }
 
-  useEffect(() => {
-    getBoardDetail(params.bno);
-  }, null);
+  // 상세 RESTAPI 요청
+  getBoardDetail(params.bno);
 
   return(
     <div className="container main">
       <h1>boardDetail {params.bno}</h1>
+      <h5>{board.title}</h5>
+      <h5>{board.content}</h5>
     </div>
   )
 }
